90d6f8618e9134c769dc096e200b1e758828c17d
BennetA <bennetakkerman@hotmail.com>
2022-10-07 14:23:10 +0200

    initial commit

---
+++ app/Http/Controllers/GroceriesController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class GroceriesController extends Controller
+{
+    public function index()
+    {
+        return "Index page";
+    }
+
+    public function create()
+    {
+    }
+    
+    public function store()
+    {
+    }
+    
+    public function edit()
+    {
+    }
+    
+    public function update()
+    {
+    }
+    
+    public function destroy()
+    {
+    }
+    
+}
